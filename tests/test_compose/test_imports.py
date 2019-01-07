d96a818ebb8e7dac5c2cd55270ecf55df6328097
Hunter Senft-Grupp <hunter@schireson.com>
2019-01-07 15:33:42 -0500

    Fix test linting errors

--- tests/test_compose/test_imports.py
+++ tests/test_compose/test_imports.py
@@ -2,16 +2,9 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    'name',
-    [
-        'Writer',
-        'write',
-        'Cell',
-        'Styler',
-        'Workbook',
-        'Worksheet',
-    ]
+    "name", ["Writer", "write", "Cell", "Styler", "Workbook", "Worksheet"]
 )
 def test_import_name(name):
     import schireson_excel.compose
+
     assert hasattr(schireson_excel.compose, name)
